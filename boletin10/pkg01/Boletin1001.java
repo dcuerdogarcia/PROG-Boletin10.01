b9005742c60bb3c32dd704d15d97563fd2eedc86
dseijasiglesias <dseijasiglesias@eniac08.danielcastelao.org>
2014-11-17 09:48:32 +0100

    Commit inicial

---
+++ boletin10/pkg01/Boletin1001.java
@@ -0,0 +1,13 @@
+//Write by Seijas
+package boletin10.pkg01;
+
+public class Boletin1001 {
+
+    public static void main(String[] args) {
+        
+           
+        
+    }
+}
+//Created by Seijas
+//To my fans and also lovers
---
+++ boletin10/pkg01/Juego.java
@@ -0,0 +1,20 @@
+//Write by Seijas
+package boletin10.pkg01;
+
+import javax.swing.JOptionPane;
+
+public class Juego {
+    int numeroAdivinar;
+    
+    
+    //Introducir o numero a adiviñar polo segundo xogador
+    public void pedirAdiviña(){
+        numeroAdivinar = Integer.parseInt(JOptionPane.showInputDialog("Introduce o número a adiviñar para o segundo xogador"));
+    }
+    
+    
+    
+    
+}
+//created by Seijas
+//To my lovers
